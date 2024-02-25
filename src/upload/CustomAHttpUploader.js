77da9d783cffb84bd6dd30736af6ee965ae79be3
imblowsnow <im.blowsnow@gmail.com>
2024-02-25 21:49:38 +0800

    feat：新增自定义上传源

--- src/upload/CustomAHttpUploader.js
+++ src/upload/CustomAHttpUploader.js
@@ -26,8 +26,7 @@ return fetch("https://pixeldrain.com/api/file/" + file.name, {
     "method": "PUT",
     "body": file
   }).then(res => res.json())
-    .then(res => "https://pixeldrain.com/u/" + res.id);
-}`},
+    .then(res => "https://pixeldrain.com/u/" + res.id);`},
         ];
     }
 }
